0d2ca7af1961279447b070ded75495d8e3b16849
Nicolas Perrin-Gilbert <perrin-isir@gmail.com>
2025-06-20 21:35:00 +0200

    simplifying the call to yomix

--- src/scanpy/tools/_yomix.py
+++ src/scanpy/tools/_yomix.py
@@ -11,31 +11,4 @@ def yomix(
     adata: AnnData,
 ):
     import yomix
-    import nest_asyncio
-    nest_asyncio.apply()
     yomix.server.start_server(adata)
-
-    # from tornado.ioloop import IOLoop
-    # from bokeh.application.handlers import FunctionHandler
-    # from bokeh.application import Application
-    # from bokeh.server.server import Server
-    # from pathlib import Path
-    #
-    # filearg = Path("/home/perrin/work/code/yomix/yomix/example/pbmc.h5ad")
-    #
-    # if True:
-    #     modify_doc = yomix.server.gen_modify_doc(filearg, None, "")
-    #
-    #     io_loop = IOLoop.current()
-    #
-    #     bokeh_app = Application(FunctionHandler(modify_doc))
-    #
-    #     server = Server({"/": bokeh_app}, io_loop=io_loop, port=5006)
-    #     server.start()
-    #
-    #     print(f"Opening Yomix on http://localhost:{5006}/\n")
-    #
-    #     io_loop.add_callback(server.show, "/")
-    #     io_loop.start()
-    #
-    # print("ok")
